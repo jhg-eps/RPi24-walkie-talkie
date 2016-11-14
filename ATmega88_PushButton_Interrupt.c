f1fa0460cd9cc206e027c07daabf9c22d04bd267
Joseph Garcia <ilive4thrills@gmail.com>
2016-11-13 23:24:59 -0500

    ATmega88_PushButton_Interrupt.c now features PCINT0 interrupt
    ATmega88_PushButton_Interrupt.c features a PCINT0 interrupt coming from pin PB0. Once the user presses the 4-pin button connected to that pin, an LED connected to PD5 lights up for one second.

--- ATmega88_PushButton_Interrupt.c
+++ ATmega88_PushButton_Interrupt.c
@@ -2,42 +2,40 @@
 #define F_CPU 1000000UL // 16 MHz clock speed
 #endif
 
-//Atmega88A default fuses
-
-//low fuse = 0x62
-//high fuse = 0xdf
-//extended action = 0xf9
+//Atmega88A default fuses //low fuse = 0x62 //high fuse = 0xdf //extended action = 0xf9
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-//PD5 = output (LED)
-//PD6 = input
-
-
-ISR(PCINT22_vect)
+ISR(PCINT0_vect)
 {
-        PORTD = 0xBF;                    // if button pressed PD5 goes high (light LED), PD6 is set low
-        delay(5000);
+        if (!(PINB & (1 << PINB0))) {  // see if PB0 is the pin that generated the interrupt
+            PORTD |= (1 << PORTB5);                // light up LED
+            _delay_ms(1000.00);               // delay for 5 seconds
+            PORTD &= ~(1 << PORTB5);            // turn off LED
+        }
 }
 
-void main(void)            //   1011
+void main(void)            
 {                                                             //76543210 (pin numbering)
-   PCICR |= (1 << PCIE2);            // set PCIE2 so that PCINT22 (pin PD6) can be an external interrupt input
-   //PCIFR |= (1 << PCIF2);            // When PCINT22 is triggered,  PCIF2 becomes set (one). IF SREG GIE and PCIE2 are set, MCU executes ISR. 
-                                                              // Flag is cleared when the interrupt routine is executed
-   PCMSK2 |= (1 << PCINT22);  // activate PCINT22 (PD6) as an (external) interrupt source.
+   DDRD = 0xFF;                              // PORT D is entirely set for output
+   PORTD &= 0x00;                        // PORT D is voltage low to begin with
+
+   DDRB &= ~(1 << DDB0);        // PB0 is an input pin
+   PORTB |= (1 << PORTB0);   // PB0 an input with pull-up enabled
+
+  EICRA |= 0x01;                           // external interrupt request triggered on voltage level change (no rising or falling edge consideration)
+//  EIMSK  |= 0x01;
+
+   PCICR |= (1 << PCIE0);            // set PCIE0 to enable PCMSK0 scan
+                                                               // Flag is cleared when the interrupt routine is executed
+   PCMSK0 |= (1 << PCINT0);   // set PCINT0 to trigger an interrupt on state change 
    sei();                                                // set the Global Interrupt Enable bit
-   DDRD = 0xBF;                              //PORTD output, 5 is output, 6 is input 
-   PORTD &= 0xDF;                        //PORTD '0'       set 6 high and 5 low to start
 
 while(1) 
 {
-	for(;;) {
-		// infinite loop
-	}
-        PORTD = 0xDF;                  // default condition (PD5 low, PD6, high.
+           // waiting for something to happen!!!!!
 }
 
 }
