b36ae93dc440f22f565afefc3b601e0a10b6a0a9
Joseph Garcia <ilive4thrills@gmail.com>
2016-11-20 23:03:24 -0500

    An ATmega88 pinout and the beginnings of ADC...
    Here I'm starting to implement the free-running ADC which will convert the analog signals coming from the electret microphone to digital information. The electret microphone is run through an amplifier (please see "Self biased transistor amplifier" at http://www.scienceprog.com/electret-condenser-microphone-amplifier-for-use-in-microcontroller-projects/) before it connects to the 88 at Pin PC0 (a.k.a. ADC0).

--- ATmega88_PushButton_Interrupt.c
+++ ATmega88_PushButton_Interrupt.c
@@ -26,12 +26,17 @@ void main(void)
    PORTB |= (1 << PORTB0);   // PB0 an input with pull-up enabled
 
   EICRA |= 0x01;                           // external interrupt request triggered on voltage level change (no rising or falling edge consideration)
-//  EIMSK  |= 0x01;
 
    PCICR |= (1 << PCIE0);            // set PCIE0 to enable PCMSK0 scan
                                                                // Flag is cleared when the interrupt routine is executed
    PCMSK0 |= (1 << PCINT0);   // set PCINT0 to trigger an interrupt on state change 
    sei();                                                // set the Global Interrupt Enable bit
+
+  ADCSRA |= ((1 << ADPS1) | (1 << ADPS0) | (1 << ADATE));    // set the ADC clock prescaler to 8, enable ADC auto-triggering
+  ADMUX |= (1 << REFS0);      // set AVCC (VCC) as our ADC reference voltage (relative to ground)
+  ADCSRB &= ~((1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0));  //set up free-running ADC now that ADATE is enabled
+  ADCSRA |= (1 << ADEN);                          // enable ADC!!!
+  ADCSRA |= (1 << ADSC);                          // start doing those AD conversions!!!!
 
 while(1) 
 {
